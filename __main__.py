65d36993df8ad76e99ee063e10019415e0579da7
joshuarreid <60335156+joshuarreid@users.noreply.github.com>
2020-12-04 01:28:46 -0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -5,7 +5,7 @@ from traceback import format_exc
 
 while True:
     try:
-        bot = Bot("SmartBot" , bot_id)
+        bot = Bot("SmartBot", bot_id)
     except :
         client.bots.post(bot_id=bot_id, text="Uh Oh! Something went wrong!")
         exceptiondata = format_exc().splitlines()
